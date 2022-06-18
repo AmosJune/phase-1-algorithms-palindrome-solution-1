4be82651580e950f2caeb2a7e1f3dead4e88f3c3
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-06-18 15:25:12 +0300

    commit tree d86385a398ef50809d421781e5ef90a453fb62b9

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-function isPalindrome(word) {
+function reverseString(word){
+  return word.split('').reverse().join('');
+}; 
+
+function isPalindrome(word){
   // Write your algorithm here
-}
+  const reversedWord = reverseString(word)
+  return word === reversedWord;
+};
 
 /* 
   Add your pseudocode here
+
+Split the string, reverse it and join it
+and if the reverseString is the same then
+return true, else false;
+
 */
 
 /*
   Add written explanation of your solution here
+  isPalindrome is a function that returns true
+  if the string is a palindrome(same backward as it reads forward)
+  or false if the criteria is not met.
+
 */
 
 // You can run `node index.js` to view these console logs
